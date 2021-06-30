36e2c1cef8bd832ec2b5a6f73e4e8d3b8d7faa3a
oqlaalrefai <2015978054@ses.yu.edu.jo>
2021-06-30 23:35:57 +0300

    lab9

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 let hours = ['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm','8pm'];
 
@@ -156,3 +156,15 @@ for (let i = 0; i < hours.length; i++) {
   finaltd.textContent=megaTotal;
 }
 makefooter();
+function addNewStore(event){
+  event.preventDefault();
+  let newStore = event.target.newstorename.value;
+  let newMin = event.target.min.value;
+  let newMax = event.target.max.value;
+  let newAvg = event.target.avg.value;
+
+  let newStoreAdd = new StoreSales(newMin, newMax, newAvg, newStore);
+console.log(newStoreAdd)
+
+
+}
